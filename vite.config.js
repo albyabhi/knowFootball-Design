3092f9cca4485d532d0ef562b9a1ee8a2f410616
Alby <alby4645@gmail.com>
2024-12-17 14:18:09 +0530

    5 commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@mui/material', '@emotion/react', '@emotion/styled'], // Externalize MUI and Emotion
+      external: ['@mui/material', '@emotion/react', '@emotion/styled'], 
     },
   },
 });
